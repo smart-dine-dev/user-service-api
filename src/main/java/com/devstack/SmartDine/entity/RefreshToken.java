a24cf96a44984a0c1bc428c4406d8276af0395db
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-22 21:24:54 +0530

    4

---
+++ src/main/java/com/devstack/SmartDine/entity/RefreshToken.java
@@ -0,0 +1,48 @@
+package com.devstack.SmartDine.entity;
+
+import com.devstack.SmartDine.entity.enums.OtpType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name="refresh_token")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name="token", unique = true, nullable = false, columnDefinition="TEXT")
+    private String token;
+
+    @Column(name="revoked", nullable = false)
+    @Builder.Default
+    private boolean revoked=false;
+
+    @Column(name="expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalTime createdAt;
+
+    public boolean isExpired(){
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+    public boolean isValid(String code){
+        return !revoked && !isExpired();
+    }
+}
