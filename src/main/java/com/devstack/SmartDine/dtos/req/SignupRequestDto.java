0d309dc3de4d8a3d8520f28e89be5c8aadac4e1e
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-25 20:57:03 +0530

    5

---
+++ src/main/java/com/devstack/SmartDine/dtos/req/SignupRequestDto.java
@@ -0,0 +1,31 @@
+package com.devstack.SmartDine.dtos.req;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignupRequestDto {
+    @NotBlank(message = "First name is required!")
+    @Size(max = 100, message = "first name must not exceed 100 chars")
+    private String firstName;
+    @NotBlank(message = "Last name is required!")
+    @Size(max = 100, message = "last name must not exceed 100 chars")
+    private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+    @NotBlank(message = "Number is required")
+    private String phoneNumber;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}
