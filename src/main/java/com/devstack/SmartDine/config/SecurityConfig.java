0c6b0976563b9519aeeb514a13d4a96b98ebb8d5
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-25 20:45:48 +0530

    5

--- src/main/java/com/devstack/SmartDine/config/SecurityConfig.java
+++ src/main/java/com/devstack/SmartDine/config/SecurityConfig.java
@@ -5,14 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_PATHS={
+    private static final String[] PUBLIC_PATHS = {
             "/api/auth/signup",
             "/api/auth/login",
             "/api/auth/login/google",
@@ -24,20 +32,35 @@ public class SecurityConfig {
     };
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-        http.csrf(csrf->csrf.disable())
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
                 .sessionManagement(
-                        sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth->auth.requestMatchers(PUBLIC_PATHS).permitAll()
+                        sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(PUBLIC_PATHS).permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oath2->oath2
-                        .jwt(jwt->jwt.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter())))
+                .oauth2ResourceServer(oath2 -> oath2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter())));
+        return http.build();
 
     }
 
-    private JwtAuthenticationConvertor keycloakJwtAuthenticationConverter(){
-        J
+    private JwtAuthenticationConverter keycloakJwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(this::extractAuthorities);
+        return converter;
+    }
+
+    private Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess == null || realmAccess.get("roles") == null) {
+            return List.of();
+        }
+        List<String> roles = (List<String>) realmAccess.get("roles");
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(("ROLE_" + role.toLowerCase())))
+                .collect(Collectors.toList());
+
     }
 
 }
