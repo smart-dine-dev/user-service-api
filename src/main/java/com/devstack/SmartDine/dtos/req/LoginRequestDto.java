75d417dfa3d149db48d7e12f934569f596af2b2a
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-25 21:07:02 +0530

    6

---
+++ src/main/java/com/devstack/SmartDine/dtos/req/LoginRequestDto.java
@@ -0,0 +1,18 @@
+package com.devstack.SmartDine.dtos.req;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
