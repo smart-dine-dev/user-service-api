5b37a86373aa1e660b2ecbedfa1023aea83f629a
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-27 21:41:56 +0530

    15

--- src/main/java/com/devstack/SmartDine/adviser/GlobalExceptionHandler.java
+++ src/main/java/com/devstack/SmartDine/adviser/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.devstack.SmartDine.exceptions;
+package com.devstack.SmartDine.adviser;
 
+import com.devstack.SmartDine.exceptions.BaseException;
+import com.devstack.SmartDine.exceptions.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
