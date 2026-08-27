670a95d8dd46dcf954673080dbb5748252a07908
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-27 21:10:47 +0530

    10

--- src/main/java/com/devstack/SmartDine/config/RedisConfig.java
+++ src/main/java/com/devstack/SmartDine/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.devstack.SmartDine.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,25 +7,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-
-        // Assign the automatic connection factory provided by Spring Boot
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-
-        // Use StringSerializer for keys and hash keys
         template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-
-        // Use JSON serializer for values and hash values
-        // template.setValueSerializer(new GenericJacksonJsonRedisSerializer());
-        // template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        template.afterPropertiesSet();
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
-
     }
 }
