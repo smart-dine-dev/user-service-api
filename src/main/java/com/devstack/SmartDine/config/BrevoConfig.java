48ec93b84822442d99607c83eefee56ef2d6a688
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-27 20:23:32 +0530

    7

--- src/main/java/com/devstack/SmartDine/config/BrevoConfig.java
+++ src/main/java/com/devstack/SmartDine/config/BrevoConfig.java
@@ -2,11 +2,14 @@ package com.devstack.SmartDine.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@EnableConfigurationProperties(BrevoConfig.BrevoProperties.class)
 public class BrevoConfig {
     private static final String BREVO_BASE_URL="https://api.brevo.com/v3";
 
@@ -14,11 +17,14 @@ public class BrevoConfig {
     public RestClient brevoRestClient(BrevoProperties bravo){
         return RestClient.builder()
                 .baseUrl(BREVO_BASE_URL)
-                .defaultHeader("api-key", bravo.apiKey)
+                .defaultHeader("api-key", bravo.getApiKey())
                 .defaultHeader("Content-Type","application/json")
                 .defaultHeader("Accept","application/json")
                 .build();
     }
+    @Getter
+    @Setter
+    @ConfigurationProperties(prefix = "brevo")
     public static class BrevoProperties{
         private String apiKey;
         private Sender sender = new Sender();
