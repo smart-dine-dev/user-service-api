712b2413a02fe215a32c2eb0d5f46a37e32dd3af
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-29 21:14:34 +0530

    20

--- src/main/java/com/devstack/SmartDine/util/KeyCloakUtil.java
+++ src/main/java/com/devstack/SmartDine/util/KeyCloakUtil.java
@@ -1,4 +1,73 @@
 package com.devstack.SmartDine.util;
 
+import com.devstack.SmartDine.config.KeyCloakConfig;
+import com.devstack.SmartDine.exceptions.KeyCloakIntegrationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+import java.rmi.Remote;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
 public class KeyCloakUtil {
+    private final RestClient keyCloakRestClient;
+    private final KeyCloakConfig.KeyCloakProperties properties;
+
+    private String createUser(String email, String password, String firstName, String lastName){
+        String adminToken = getAdminToken();
+
+        Map<String, Object> userPresentation = Map.of(
+                "username", email,
+                "email",email,
+                "firstName", firstName,
+                "lastName", lastName,
+                "enabled",true,
+                "emailVerified",false,
+                "credentials", List.of(Map.of(
+                        "type","password",
+                        "value", password,
+                        "temporary",false
+                ))
+        );
+
+        var response = keyCloakRestClient.post()
+                .uri("/admin/realms/{realm}/users",properties.getRealm())
+                .header("Authorization", "Bearer "+adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(userPresentation)
+                .retrieve()
+                .toBodilessEntity();
+        String location = response.getHeaders().getFirst("Location");
+        if(location == null){
+            throw new KeyCloakIntegrationException("location not found");
+        }
+        return location.substring(location.lastIndexOf("/")+1);
+    }
+
+
+    private String getAdminToken(){
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("grant_type","password");
+        form.add("client_id","admin-cli");
+        form.add("username",properties.getAdmin().getUsername());
+        form.add("password",properties.getAdmin().getPassword());
+
+        Map<String, Object> response = keyCloakRestClient.post()
+                .uri("/realms/master/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(form)
+                .retrieve()
+                .body( new ParameterizedTypeReference<>() {});
+
+        return (String) response.get("access_token");
+
+    }
+
 }
